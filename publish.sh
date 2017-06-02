4470060d877886c1edb30afb02be916e5d595261
Savpek <savolainen.pekka@gmail.com>
2017-06-02 15:35:18 +0300

    Added publish.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd Protacon.PowerBi.Token.Middleware
 echo $VERSION.$TRAVIS_BUILD_NUMBER
-dotnet nuget pack Protacon.PowerBi.Token.Middleware.nuspec -Version $VERSION.$TRAVIS_BUILD_NUMBER -IncludeReferencedProjects -Prop Configuration=Release
+dotnet pack --version $VERSION.$TRAVIS_BUILD_NUMBER --IncludeReferencedProjects --Prop Configuration=Release
 dotnet nuget push *.nupkg $NUGET_API_KEY -verbosity detailed
